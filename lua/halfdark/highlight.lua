b4a2ef33dc1e76ee024b89a0d5a45abb75d22392
Martin Walls <martin_w@optimove.com>
2025-11-03 20:59:23 +0000

    add basic highlight groups

--- lua/halfdark/highlight.lua
+++ lua/halfdark/highlight.lua
@@ -2,8 +2,38 @@ local M = {}
 local palette = require("halfdark.palette")
 
 local theme = {
+    -- BASELINE --
+    Normal = { bg = palette.bg0, fg = palette.fg0 },
     Comment = { fg = palette.grey, bg = palette.black },
-    Normal = { bg = palette.bg0 },
+
+    Conceal = { fg = palette.bg2 },
+    CurSearch = { bg = palette.bg_yellow, fg = palette.black },
+
+    DiffAdd = { bg = palette.green, fg = palette.black },
+    DiffChange = { bg = palette.bg_blue, fg = palette.black },
+    DiffDelete = { bg = palette.red, fg = palette.black },
+    DiffText = { bg = palette.bg2, fg = palette.fg1 },
+
+    ColorColumn = { bg = palette.bg1 },
+    CursorColumn = { bg = palette.bg1 },
+    CursorLine = { bg = palette.bg1 },
+    EndOfBuffer = { fg = palette.grey },
+
+    -- THEME VARIABLES                                    --
+    -- Single points of truth for other groups to link to --
+    HalfdarkPunctuation = { fg = palette.grey },
+
+    -- BASELINE SYNTAX                 --
+    -- Treesitter groups link to these --
+    Constant = { fg = palette.yellow },
+    Boolean = { link = "Constant" },
+    Character = { link = "Constant" },
+    Float = { link = "Constant" },
+    Number = { link = "Constant" },
+    String = { fg = palette.green },
+    Operator = { link = "HalfdarkPunctuation" },
+
+    -- LSP --
 }
 
 function M.setup()
