27f58450411ab562a738128edc7b896ffe12c4b0
Martin Walls <martin_w@optimove.com>
2025-11-04 13:09:54 +0000

    tweak lualine theme

--- lua/lualine/themes/halfdark.lua
+++ lua/lualine/themes/halfdark.lua
@@ -4,7 +4,7 @@ return {
     normal = {
         a = { fg = palette.bg500, bg = palette.green500, gui = "bold" },
         b = { fg = palette.fg500, bg = palette.bg700 },
-        c = { fg = palette.fg500, bg = palette.bg600 },
+        c = { fg = palette.grey700, bg = palette.bg600 },
     },
     inactive = {
         a = { fg = palette.grey500, bg = palette.bg400, gui = "bold" },
