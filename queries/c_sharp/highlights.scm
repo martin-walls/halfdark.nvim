ebca82fda65fc25e12322c39c20773e63f0d91f0
Martin Walls <martin_w@optimove.com>
2025-11-05 11:17:32 +0000

    c#: top level defns for record, struct, enum

--- queries/c_sharp/highlights.scm
+++ queries/c_sharp/highlights.scm
@@ -3,6 +3,15 @@
 (class_declaration
   name: (identifier) @halfdark.topLevelDefn (#set! priority 150))
 
+(record_declaration
+  name: (identifier) @halfdark.topLevelDefn (#set! priority 150))
+
+(struct_declaration
+  name: (identifier) @halfdark.topLevelDefn (#set! priority 150))
+
+(enum_declaration
+  name: (identifier) @halfdark.topLevelDefn (#set! priority 150))
+
 (method_declaration
   name: (identifier) @halfdark.topLevelDefn (#set! priority 150))
 
