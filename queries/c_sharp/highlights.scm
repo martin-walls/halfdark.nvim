03ea3660b69db2e3ff578ac5f28924e9bddb2f5f
Martin Walls <martin_w@optimove.com>
2025-11-06 11:49:27 +0000

    c#: cover missing query cases

--- queries/c_sharp/highlights.scm
+++ queries/c_sharp/highlights.scm
@@ -2,24 +2,27 @@
 
 (class_declaration
   name: (identifier) @halfdark.definition (#set! priority 150))
-
+(interface_declaration
+  name: (identifier) @halfdark.definition (#set! priority 150))
 (record_declaration
   name: (identifier) @halfdark.definition (#set! priority 150))
-
 (struct_declaration
   name: (identifier) @halfdark.definition (#set! priority 150))
-
 (enum_declaration
   name: (identifier) @halfdark.definition (#set! priority 150))
-
 (method_declaration
+  name: (identifier) @halfdark.definition (#set! priority 150))
+(constructor_declaration
+  name: (identifier) @halfdark.definition (#set! priority 150))
+(destructor_declaration
   name: (identifier) @halfdark.definition (#set! priority 150))
 
 (variable_declarator
   name: (identifier) @halfdark.variableDefinition (#set! priority 150))
+(property_declaration
+  name: (identifier) @halfdark.variableDefinition (#set! priority 150))
 
 (property_declaration
   type: (predefined_type) @halfdark.declarationType (#set! priority 150))
-
 (variable_declaration
   type: (predefined_type) @halfdark.declarationType (#set! priority 150))
