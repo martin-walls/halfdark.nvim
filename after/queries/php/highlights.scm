81ca464639d4169cd0375ec406411e330d668386
Martin Walls <martin_w@optimove.com>
2025-11-07 15:22:07 +0000

    php: don't highlight `self::FOO` as special

--- after/queries/php/highlights.scm
+++ after/queries/php/highlights.scm
@@ -8,3 +8,6 @@
 ;; Don't highlight `$this` as Special
 (variable_name
   (name) @name (#match? @name "^this$")) @halfdark.normal
+;; Don't highlight `self` in `self::FOO` as Special
+(class_constant_access_expression
+  (relative_scope) @halfdark.normal (#match? @halfdark.normal "^self$"))
