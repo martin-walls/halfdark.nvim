46a47c0079dd3851e7cdc3bf2d229b665709f82d
Martin Walls <martin_w@optimove.com>
2025-11-07 10:47:19 +0000

    typescript queries

---
+++ after/queries/typescript/highlights.scm
@@ -0,0 +1,15 @@
+;;extends
+
+(function_declaration
+  name: (identifier) @halfdark.definition (#set! priority 150))
+(enum_declaration
+  name: (identifier) @halfdark.definition (#set! priority 150))
+(class_declaration
+  name: (type_identifier) @halfdark.definition (#set! priority 150))
+(interface_declaration
+  name: (type_identifier) @halfdark.definition (#set! priority 150))
+(method_definition
+  name: (property_identifier) @halfdark.definition (#set! priority 150))
+
+(variable_declarator
+  name: (identifier) @halfdark.variableDefinition (#set! priority 150))
