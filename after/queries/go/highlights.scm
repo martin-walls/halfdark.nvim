301cc478ef6f40d90cb10422c0ccf1eab03b08d8
Martin Walls <martin_w@optimove.com>
2025-12-27 16:15:13 +0000

    go syntax queries

---
+++ after/queries/go/highlights.scm
@@ -0,0 +1,10 @@
+;;extends
+
+(function_declaration
+  name: (identifier) @halfdark.definition (#set! priority 150))
+(type_spec
+  name: (type_identifier) @halfdark.definition (#set! priority 150))
+
+(short_var_declaration
+  left: (expression_list
+          (identifier) @halfdark.variableDefinition (#set! priority 150)))
