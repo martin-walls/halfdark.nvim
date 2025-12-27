f150ba3cf58b6f2e7e89ca43bd32bf5a3e670840
Martin Walls <martin_w@optimove.com>
2025-12-27 16:20:10 +0000

    go: method declaration highlighting

--- after/queries/go/highlights.scm
+++ after/queries/go/highlights.scm
@@ -2,6 +2,8 @@
 
 (function_declaration
   name: (identifier) @halfdark.definition (#set! priority 150))
+(method_declaration
+  name: (field_identifier) @halfdark.definition (#set! priority 150))
 (type_spec
   name: (type_identifier) @halfdark.definition (#set! priority 150))
 
