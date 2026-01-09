5e9e31818cffcfc36cfbebf85e8c2cbcdc52db4e
Martin Walls <martin_w@optimove.com>
2026-01-09 14:12:05 +0000

    rust: better doc comment highlighting

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -8,3 +8,5 @@
 ; Don't highlight start of attribute as "special" punctuation
 (attribute_item
   "#" @halfdark.punctuation (#set! priority 150))
+
+((doc_comment) @halfdark.docComment (#set! priority 150))
